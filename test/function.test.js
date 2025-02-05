09ddffca6757f382ae7bd6528f6394e78dbad135
Sacha <sachaobiedzy@gmail.com>
2025-02-05 10:10:07 +0100

    Chore: merge des commits

--- test/function.test.js
+++ test/function.test.js
@@ -1,5 +1,22 @@
 const { isEven, calculateTotalPrice, processPurchase, sendNotification, generatePassword } = require("./../src/function.js");
 
+describe('On vérifie si un nombre est paire ou impaire', () => {
+    test("si j'entre un chiffre pair, alors je devrais avoir true", () => {
+        expect(isEven(2)).toBe(true);
+        expect(isEven(0)).toBe(true);
+        expect(isEven(-2)).toBe(true);
+    });
+
+    test("Si j'entre un chiffre impaire, alors je devris avoir false", () => {
+        expect(isEven(3)).toBe(false);
+        expect(isEven(-3)).toBe(false);
+    });
+
+    test("si j'entre un caractère, alors je devrais avoir une erreur", () => {
+        expect(() => isEven("a")).toThrow("Input must be a number");
+    });
+});
+
 describe('On calcul le prix total', () => {
     test('Le panier n\'est pas une liste', () => {
         expect(() => calculateTotalPrice(12, 2).toThrowError('Prices must be an array'));
@@ -19,7 +36,23 @@ describe('On calcul le prix total', () => {
     });
 });
 
+describe('On envoie une notification', () => {
+    test("Si j'entre un string, alors je devrais avoir une notification", () => {
+        console.log = jest.fn();
+        sendNotification("test");
+        expect(console.log).toHaveBeenCalledWith("Notification envoyée : test");
+    });
+});
+
 describe('On achète le panier', () => {
+    test('Le prix est calculé et une notification est envoyé', () => {
+        const calculateTotalPrice = jest.fn();
+        const sendNotification = jest.fn();
+
+        expect(() => processPurchase([15, 12, 13], 2).toBeCalled(calculateTotalPrice));
+        expect(() => processPurchase([15, 12, 13], 2).toBeCalled(sendNotification));
+    });
+
     test('Le panier n\'est pas une liste', () => {
         expect(() => processPurchase(12, 2).toThrowError('Prices must be an array'));
     });
@@ -38,70 +71,42 @@ describe('On achète le panier', () => {
     });
 });
 
-describe('On vérifie si un nombre est paire ou impaire', () => {
-    test("si j'entre 2, alors je devris avoir true", () => {
-        expect(isEven(2)).toBe(true);
-    });
-
-    test("Si j'entre 3, alors je devris avoir false", () => {
-        expect(isEven(3)).toBe(false);
-    });
-
-    test("si j'entre un caractère, alors je devris avoir une erreur", () => {
-        expect(() => isEven("a")).toThrow("Input must be a number");
-    });
-
-    test("si je rentre un 0, alors je devrais avoir true", () => {
-        expect(isEven(0)).toBe(true);
-    });
-
-    test("si je rentre un -2, alors je devrais avoir true", () => {
-        expect(isEven(-2)).toBe(true);
-    });
-});
-
-describe('On envoie une notification', () => {
-    test("Si j'entre un string, alors je devrais avoir une notification", () => {
-        console.log = jest.fn();
-        sendNotification("test");
-        expect(console.log).toHaveBeenCalledWith("Notification envoyée : test");
-    });
-});
-
 describe('On génère un mot de passe', () => {
+    test("La longueur est un nombre positif supérieur à 5", () => {
+        expect(() => generatePassword(-1)).toThrowError('Length must be a number greater than or equal to 6');
+        expect(() => generatePassword(5)).toThrowError('Length must be a number greater than or equal to 6');
+        expect(() => generatePassword('1')).toThrowError('Length must be a number greater than or equal to 6');
+    });
+
     test("Si je rentre 6, alors je devrais avoir un mot de passe de 6 caractères", () => {
         expect(generatePassword(6)).toHaveLength(6);
     });
 
-    test("Si je rentre 6 avec l'option uppercase, alors je devrais avoir un mot de passe de 6 caractères avec une majuscule", () => {
+    test("Si je rentre 6 avec l'option uppercase, alors je devrais avoir un mot de passe de 6 caractères qui peut avoir une majuscule", () => {
         expect(generatePassword(6, { uppercase: true })).toMatch(/[A-Za-z]/);
     });
 
-    test("Si je rentre 6 avec l'option numbers, alors je devrais avoir un mot de passe de 6 caractères avec un chiffre", () => {
+    test("Si je rentre 6 avec l'option numbers, alors je devrais avoir un mot de passe de 6 caractères qui peut avoir un chiffre", () => {
         expect(generatePassword(6, { numbers: true })).toMatch(/[a-z0-9]/);
     });
 
-    test("Si je rentre 6 avec l'option specialChars, alors je devrais avoir un mot de passe de 6 caractères avec un caractère spécial", () => {
+    test("Si je rentre 6 avec l'option specialChars, alors je devrais avoir un mot de passe de 6 caractères qui peut avoir un caractère spécial", () => {
         expect(generatePassword(6, { specialChars: true })).toMatch(/[!@#$% ^&*()\-+={}[\]:;"'<>,.?\/ |\\a-z]/);
     });
 
-    test("Si je rentre 6 avec l'option uppercase, numbers, specialChars, alors je devrais avoir un mot de passe de 6 caractères avec une majuscule, un chiffre et un caractère spécial", () => {
+    test("Si je rentre 6 avec l'option uppercase, numbers, specialChars, alors je devrais avoir un mot de passe de 6 caractères qui peut avoir une majuscule, un chiffre et un caractère spécial", () => {
         expect(generatePassword(6, { uppercase: true, numbers: true, specialChars: true })).toMatch(/[!@#$% ^&*()\-+={}[\]:;"'<>,.?\/ |\\a-zA-Z]/);
     });
 
-    test("Si je rentre 6 avec l'option uppercase, numbers, alors je devrais avoir un mot de passe de 6 caractères avec une majuscule et un chiffre", () => {
+    test("Si je rentre 6 avec l'option uppercase, numbers, alors je devrais avoir un mot de passe de 6 caractères qui peut avoir une majuscule et un chiffre", () => {
         expect(generatePassword(6, { uppercase: true, numbers: true })).toMatch(/[A-Za-z0-9]/);
     });
 
-    test("Si je rentre 6 avec l'option uppercase, specialChars, alors je devrais avoir un mot de passe de 6 caractères avec une majuscule et un caractère spécial", () => {
+    test("Si je rentre 6 avec l'option uppercase, specialChars, alors je devrais avoir un mot de passe de 6 caractères qui peut avoir une majuscule et un caractère spécial", () => {
         expect(generatePassword(6, { uppercase: true, specialChars: true })).toMatch(/[!@#$% ^&*()\-+={}[\]:;"'<>,.?\/ |\\a-zA-Z]/);
     });
 
-    test("Si je rentre 6 avec l'option numbers, specialChars, alors je devrais avoir un mot de passe de 6 caractères avec un chiffre et un caractère spécial", () => {
+    test("Si je rentre 6 avec l'option numbers, specialChars, alors je devrais avoir un mot de passe de 6 caractères qui peut avoir un chiffre et un caractère spécial", () => {
         expect(generatePassword(6, { numbers: true, specialChars: true })).toMatch(/[!@#$% ^&*()\-+={}[\]:;"'<>,.?\/ |\\a-z]/);
     });
 });
-
-
-
-
