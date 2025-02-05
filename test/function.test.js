0ef567eecf4a44ed4536e546b26ff2652fcf55cc
JosCousin <joshua.cousin9@gmail.com>
2025-02-05 08:27:01 +0100

    feat: ajout test isEven

--- test/function.test.js
+++ test/function.test.js
@@ -1,5 +1,23 @@
 const { isEven, calculateTotalPrice, processPurchase, sendNotification, generatePassword } = require("./../src/function.js");
 
-test("",() =>{
+// isEven
 
+test("si j'entre 2, alors je devris avoir true",() =>{
+    expect(isEven(2)).toBe(true);
+});
+
+test("Si j'entre 3, alors je devris avoir false",() =>{
+    expect(isEven(3)).toBe(false);
+});
+
+test("si j'entre un caractère, alors je devris avoir une erreur",() =>{
+    expect(() => isEven("a")).toThrow("Input must be a number");
+});
+
+test("si je rentre un 0, alors je devrais avoir true",() =>{    
+    expect(isEven(0)).toBe(true);
+});
+
+test("si je rentre un -2, alors je devrais avoir true",() =>{    
+    expect(isEven(-2)).toBe(true);
 });
