3222ec68b98c00c8c548b5f3703040056ec4490b
JosCousin <joshua.cousin9@gmail.com>
2025-02-05 09:55:59 +0100

    feat: test ajouté pour processPurchase

--- test/function.test.js
+++ test/function.test.js
@@ -36,28 +36,14 @@ describe('On achète le panier', () => {
     test('Le prix total et le prix d\'achat est le même', () => {
         expect(() => processPurchase([15,12,13],'4').toEquals(calculateTotalPrice([15,12,13],'4')))
     });
-});
-// isEven
 
-test("si j'entre 2, alors je devris avoir true",() =>{
-    expect(isEven(2)).toBe(true);
-});
-
-test("Si j'entre 3, alors je devris avoir false",() =>{
-    expect(isEven(3)).toBe(false);
+    test('La notification est bien appelé', () => {
+        console.log = jest.fn();
+        processPurchase([15,12,13], 4);
+        expect(console.log).toHaveBeenCalledWith("Notification envoyée : Votre total est de 200.00 €");
+    });
 });
 
-test("si j'entre un caractère, alors je devris avoir une erreur",() =>{
-    expect(() => isEven("a")).toThrow("Input must be a number");
-});
-
-test("si je rentre un 0, alors je devrais avoir true",() =>{    
-    expect(isEven(0)).toBe(true);
-});
-
-test("si je rentre un -2, alors je devrais avoir true",() =>{    
-    expect(isEven(-2)).toBe(true);
-});
 
 // sendNotification
 
